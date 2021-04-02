efd2b9e8572460d4a4cd06cbd2288e620b9a378e
Martin <martinjonsson01@gmail.com>
2021-04-02 09:45:05 +0200

    Adds Rating-property to Recipe

--- src/Domain/Recipes/Recipe.cs
+++ src/Domain/Recipes/Recipe.cs
@@ -5,6 +5,7 @@ namespace RecipeBook.Core.Domain.Recipes
     public class Recipe : BaseEntity
     {
         public string                    Name          { get; set; } = string.Empty;
+        public int?                      Rating        { get; set; } = null;
         public ICollection<UsedOccasion> UsedOccasions { get; set; } = new List<UsedOccasion>();
         public ICollection<Step>         Steps         { get; set; } = new List<Step>();
         public ICollection<Ingredient>   Ingredients   { get; set; } = new List<Ingredient>();
