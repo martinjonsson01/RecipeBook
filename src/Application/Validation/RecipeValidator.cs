949eb41bf86ee38677a0afad8b094aa0a192f9ae
Martin <martinjonsson01@gmail.com>
2021-04-05 11:11:08 +0200

    Adds validation error messages

---
+++ src/Application/Validation/RecipeValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+using RecipeBook.Core.Domain.Recipes;
+
+namespace RecipeBook.Core.Application.Validation
+{
+    public class RecipeValidator : AbstractValidator<Recipe>
+    {
+        private const int MaxNameLength = 100;
+        private const int MinRating     = 1;
+        private const int MaxRating     = 10;
+
+        public RecipeValidator()
+        {
+            RuleFor(recipe => recipe.Name)
+                .NotEmpty()
+                .MaximumLength(MaxNameLength)
+                .WithMessage($"Namnet måste vara under {MaxNameLength} karaktärer långt.");
+            RuleFor(recipe => recipe.Rating)
+                .InclusiveBetween(MinRating, MaxRating)
+                .WithMessage($"Betyget måste vara från {MinRating}-{MaxRating}.");
+        }
+    }
+}
---
+++ src/Application/Validation/StepsValidator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using FluentValidation;
+
+using RecipeBook.Core.Domain.Recipes;
+
+namespace RecipeBook.Core.Application.Validation
+{
+    public class StepsValidator : AbstractValidator<IList<Step>>
+    {
+        public StepsValidator()
+        {
+            RuleFor(steps => steps).Must(StartAtStepOne).WithMessage("Stegen måste börja på 1.");
+            RuleFor(steps => steps).Must(IncreaseByOne).WithMessage("Stegen måste öka med 1.");
+        }
+
+        private static bool IncreaseByOne(IList<Step> steps)
+        {
+            for (var i = 1; i < steps.Count; i++)
+            {
+                Step previous = steps[i - 1];
+                Step current  = steps[i];
+                if (current.Number - previous.Number != 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool StartAtStepOne(IList<Step> steps)
+        {
+            if (steps.Count == 0) return false;
+            return steps[0].Number == 1;
+        }
+    }
+
+    internal class StepValidator : AbstractValidator<Step>
+    {
+        
+    }
+}
