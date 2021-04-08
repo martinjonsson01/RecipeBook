9ab50d0259b022862b9e7c2a5936a066ff577e2b
Martin <martinjonsson01@gmail.com>
2021-04-08 10:24:56 +0200

    Adds ImagePath to Recipe-class

--- src/Domain/Recipes/Recipe.cs
+++ src/Domain/Recipes/Recipe.cs
@@ -6,6 +6,7 @@ namespace RecipeBook.Core.Domain.Recipes
     {
         public string              Name          { get; set; } = string.Empty;
         public int?                Rating        { get; set; } = null;
+        public string?             ImagePath     { get; set; } = null;
         public IList<UsedOccasion> UsedOccasions { get; set; } = new List<UsedOccasion>();
         public IList<Step>         Steps         { get; set; } = new List<Step>();
         public IList<Ingredient>   Ingredients   { get; set; } = new List<Ingredient>();
