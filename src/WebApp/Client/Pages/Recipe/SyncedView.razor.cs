0be94132d18546e7a41ad707d3861d3a865a3eb5
Martin <martinjonsson01@gmail.com>
2021-04-16 17:47:28 +0200

    Refactors out shared syncing logic from StepView.razor into SyncedView.razor.cs

---
+++ src/WebApp/Client/Pages/Recipe/SyncedView.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+using Microsoft.AspNetCore.Components;
+
+using RecipeBook.Core.Application.Logic;
+using RecipeBook.Core.Domain;
+
+namespace RecipeBook.Presentation.WebApp.Client.Pages.Recipe
+{
+    public partial class SyncedView<TResource> : ComponentBase
+        where TResource : IShallowCloneable<TResource>, IEquatable<TResource>
+    {
+        [Inject] private HttpClient Http { get; set; } = null!;
+
+        [Parameter] public string Url { get; set; } = null!;
+
+        [Parameter] public TResource Step { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback<(string, LoadStatus)> SetSaving { get; set; } = EventCallback<(string, LoadStatus)>.Empty;
+
+        protected override void OnInitialized()
+        {
+            _inputSaver = new InputSaver<TResource>(Step, Http, Url, SetSavingDelegate);
+        }
+
+        protected void ResourceHasChanged() => _inputSaver.ResourceHasChanged();
+        
+        private void SetSavingDelegate((string, LoadStatus) tuple) => SetSaving.InvokeAsync(tuple);
+
+        private InputSaver<TResource> _inputSaver = null!;
+    }
+}
