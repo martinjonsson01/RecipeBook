e0274183ecc1778451845b7d5d8378d39b0b3b19
Martin <martinjonsson01@gmail.com>
2021-04-16 18:20:44 +0200

    Adds editing and syncing to ingredients list

--- src/Domain/Recipes/Ingredient.cs
+++ src/Domain/Recipes/Ingredient.cs
@@ -1,8 +1,10 @@
-﻿using RecipeBook.Core.Domain.Units;
+﻿using System;
+
+using RecipeBook.Core.Domain.Units;
 
 namespace RecipeBook.Core.Domain.Recipes
 {
-    public class Ingredient : BaseEntity
+    public class Ingredient : BaseEntity, IShallowCloneable<Ingredient>, IEquatable<Ingredient>
     {
         public string Name   { get; set; } = string.Empty;
         public Unit   Amount { get; set; } = new Mass(0);
@@ -18,6 +20,39 @@ namespace RecipeBook.Core.Domain.Recipes
                     : new Volume { Id = row.volumeid, Value = row.value }
             };
             return ingredient;
+        }
+
+        public Ingredient ShallowClone()
+        {
+            return new()
+            {
+                Id = Id,
+                Name = Name,
+                Amount = Amount
+            };
+        }
+
+        public bool Equals(Ingredient? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name && Amount.Equals(other.Amount);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Ingredient) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Name.GetHashCode() * 397) ^ Amount.GetHashCode();
+            }
         }
     }
 }
--- src/Domain/Units/Unit.cs
+++ src/Domain/Units/Unit.cs
@@ -1,7 +1,29 @@
-﻿namespace RecipeBook.Core.Domain.Units
+﻿using System;
+
+namespace RecipeBook.Core.Domain.Units
 {
-    public abstract class Unit : BaseEntity
+    public abstract class Unit : BaseEntity, IEquatable<Unit>
     {
         public double Value { get; set; }
+
+        public bool Equals(Unit? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Unit) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
     }
 }
