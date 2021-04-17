caf9dd46d5a31d97a01af0766fed3bae88e91e0a
Martin <martinjonsson01@gmail.com>
2021-04-17 12:19:36 +0200

    Refactors out javascript interop-functions to interop.js

---
+++ src/WebApp/Client/wwwroot/js/interop.js
@@ -0,0 +1,11 @@
+﻿window.interopFunctions = {
+    textareaAutoHeight: function (textarea) {
+        textarea.style.height = "5px";
+        textarea.style.height = (textarea.scrollHeight) + "px";
+    },
+    copyElementHeight: function (fromId, toId) {
+        let from = document.getElementById(fromId);
+        let to = document.getElementById(toId);
+        to.style.height = window.getComputedStyle(from).getPropertyValue('height');
+    }
+}
