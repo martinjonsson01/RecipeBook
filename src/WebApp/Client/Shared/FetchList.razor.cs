142d9bbcd6db827f84c0af9cc4b7c56e95b40461
Martin <martinjonsson01@gmail.com>
2021-05-01 16:05:30 +0200

    Adds support for custom "Create new item"-button in FetchList.razor

--- src/WebApp/Client/Shared/FetchList.razor.cs
+++ src/WebApp/Client/Shared/FetchList.razor.cs
@@ -76,6 +76,7 @@ namespace RecipeBook.Presentation.WebApp.Client.Shared
                 default:
                     throw new ArgumentOutOfRangeException(nameof(e));
             }
+            StateHasChanged();
         }
 
 
