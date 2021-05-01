fa56df4f91745b271314981ceebc268c33003fdf
Martin <martinjonsson01@gmail.com>
2021-05-01 11:58:28 +0200

    Rewrites layout of IngredientsView.razor to use CSS instead of JS-interop for aligning items and labels

--- src/WebApp/Client/wwwroot/js/interop.js
+++ src/WebApp/Client/wwwroot/js/interop.js
@@ -2,10 +2,5 @@
     textareaAutoHeight: function (textarea) {
         textarea.style.height = "5px";
         textarea.style.height = (textarea.scrollHeight) + "px";
-    },
-    copyElementHeight: function (fromId, toId) {
-        let from = document.getElementById(fromId);
-        let to = document.getElementById(toId);
-        to.style.height = window.getComputedStyle(from).getPropertyValue('height');
     }
 }
